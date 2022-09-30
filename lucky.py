e3f5088b16469d4136c42ad036f4638f23016597
pavkyr <kyrylenkopa2001@gmail.com>
2022-09-30 03:34:32 +0300

    Update lucky.py

--- lucky.py
+++ lucky.py
@@ -8,3 +8,16 @@ for Sierentz Global Merchants
 
 Test task
 """
+
+import re 
+
+def fun(num:int)->list:
+  result=[]
+  match=re.findall(r'([5-6]{2,})',str(num))
+  if len(match)==0:
+    return 0
+  for i in match:
+    lenght=len(i)
+    if i != lenght*'5' and i !=lenght*'6':
+      result.append(i)
+  return int(max(result))
