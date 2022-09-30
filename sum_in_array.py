e9dff2ca017e6b63b8cdf1d56e0b4f70938c4460
pavkyr <kyrylenkopa2001@gmail.com>
2022-09-30 03:35:38 +0300

    Update sum_in_array.py

--- sum_in_array.py
+++ sum_in_array.py
@@ -8,3 +8,19 @@ for Sierentz Global Merchants
 
 Test task
 """
+#complexity: O(n)
+#memory: O(1)
+
+def f(array:list,res:int)->list:
+  left_p,right_p=0,len(l)-1
+
+  while left_p<right_p:
+    sum=l[left_p]+l[right_p]
+    if sum==res:
+      return [l[left_p],l[right_p]]
+    if sum<res:
+      left_p+=1
+    else:
+      right_p-=1
+
+  return [-1]
